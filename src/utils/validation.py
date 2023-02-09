c4a9ef988fe77c08140d84a9c05b2edb69d40996
phuc16102001 <phuc16102001@gmail.com>
2023-02-09 16:33:24 +0700

    Add validation function

---
+++ src/utils/validation.py
@@ -0,0 +1,24 @@
+import torch
+from mask import create_mask
+from ..config.config import config
+import numpy as np
+
+def validiate(model, valid_iter, criterion, src_pad, trg_pad):
+    model.eval()
+    
+    with torch.no_grad():
+        total_loss = []
+        for batch in valid_iter:
+            src = batch.src.transpose(0,1).cuda()
+            trg = batch.trg.transpose(0,1).cuda()
+            trg_input = trg[:, :-1]
+            
+            src_mask = create_mask(src, src_pad, False, config['device'])
+            trg_mask = create_mask(trg_input, trg_pad, True, config['device'])
+            preds = model(src, trg_input, src_mask, trg_mask)
+            ys = trg[:, 1:].contiguous().view(-1)
+            
+            loss = criterion(preds.view(-1, preds.size(-1)), ys)
+            loss = loss.item()
+            total_loss.append(loss)
+    return np.mean(total_loss)
