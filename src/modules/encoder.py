7cba3803713f85319ecf1df01547682a73c2bf23
phuc16102001 <phuc16102001@gmail.com>
2023-02-09 16:24:27 +0700

    Add encoder

---
+++ src/modules/encoder.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+from ..utils.cloner import get_clones
+from embedder import Embedder
+from positional_encoding import PositionalEncoder
+from encoder_layer import EncoderLayer
+from norm import Norm
+
+class Encoder(nn.Module):
+    def __init__(self, vocab_size, d_model, n, heads, dropout=0.1):
+        super().__init__()
+        
+        self.n = n
+        self.embed = Embedder(vocab_size, d_model)
+        self.pe = PositionalEncoder(d_model, dropout=dropout)
+        self.encoder_layers = get_clones(
+            EncoderLayer(d_model, heads, dropout),
+            n
+        )
+        self.norm = Norm(d_model)
+    
+    def forward(self, x, mask):
+        x = self.embed(x)
+        x = self.pe(x)
+        for i in range(self.n):
+            x = self.encoder_layers[i](x, mask)
+        x = self.norm(x)
+        return x
