abaf5dc246dbcda54191df0eeb0a37f4e0afc9c0
phuc16102001 <phuc16102001@gmail.com>
2023-02-09 15:35:15 +0700

    Update libraries installation code

---
+++ libraries.sh
@@ -0,0 +1,5 @@
+pip -q install torchtext==0.6.0
+pip -q install spacy
+pip -q install pyvi 
+pip -q install https://gitlab.com/trungtv/vi_spacy/-/raw/master/vi_core_news_lg/dist/vi_core_news_lg-0.0.1.tar.gz    
+python -m spacy download en_core_web_sm -q
