81df8c208f816d0efe6ce4d7028a0b7428782a7d
phuc16102001 <phuc16102001@gmail.com>
2023-02-10 00:00:03 +0700

    update inference code

--- src/inference.py
+++ src/inference.py
@@ -1,9 +1,21 @@
 import torch
+from modules.transformer import Transformer
+from config.config import config
 
 def main():
-    src = torch.load('../models/src_vocab.pth')
-    trg = torch.load('../models/trg_vocab.pth')
-    model = torch.load('../models/model_best.pth')
+    src_field = torch.load('../models/src_vocab.pth')
+    trg_field = torch.load('../models/trg_vocab.pth')
+    d_model = config['d_model']
+
+    model = Transformer(
+        len(src_field.vocab),
+        len(trg_field.vocab),
+        d_model = d_model,
+        n = 6,
+        heads = 8,
+        dropout = 0.1
+    )
+    model.load_state_dict(torch.load('../models/model_best.pth'))
 
 if __name__=="__main__":
     main()
