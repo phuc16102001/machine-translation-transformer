d5c98ac7693f493df89cb07153544a8194d9acc9
phuc16102001 <phuc16102001@gmail.com>
2023-02-09 15:30:30 +0700

    Upload data and reader

---
+++ src/config/config.py
@@ -0,0 +1,5 @@
+config = {
+    'max_strlen': 120,
+    'batch_size': 1500,
+    'd_model': 512
+}
---
+++ src/utils/reader.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def create_data(src_path, trg_path):
+    src_data = open(src_path).read().strip().split('\n')
+    trg_data = open(trg_path).read().strip().split('\n')
+
+    df = pd.DataFrame({
+        'src': [line for line in src_data],
+        'trg': [line for line in trg_data]
+    })
+    not_empty = ((df['src'].apply(len)>0) & (df['trg'].apply(len)>0))
+    df = df[not_empty]
+    return df
+
+if __name__=="__main__":
+    df_train = create_data('data/train.vi', 'data/train.en')
+    df_val = create_data('data/tst2013.vi', 'data/tst2013.en')
+
+    print(f"Train size: {len(df_train)}")
+    print(f"Validate size: {len(df_val)}")
