ed911112c07454fdff4b63a467c1eff8909d9ea1
phuc16102001 <phuc16102001@gmail.com>
2023-02-09 15:43:40 +0700

    Add field code

--- src/utils/field.py
+++ src/utils/field.py
@@ -1,20 +1,14 @@
-import pandas as pd
+from torchtext.data import field
 
-def create_data(src_path, trg_path):
-    src_data = open(src_path).read().strip().split('\n')
-    trg_data = open(trg_path).read().strip().split('\n')
-
-    df = pd.DataFrame({
-        'src': [line for line in src_data],
-        'trg': [line for line in trg_data]
-    })
-    not_empty = ((df['src'].apply(len)>0) & (df['trg'].apply(len)>0))
-    df = df[not_empty]
-    return df
-
-if __name__=="__main__":
-    df_train = create_data('data/train.vi', 'data/train.en')
-    df_val = create_data('data/tst2013.vi', 'data/tst2013.en')
-
-    print(f"Train size: {len(df_train)}")
-    print(f"Validate size: {len(df_val)}")
+def create_field(src_tokenizer, trg_tokenizer):
+    src_field = field.Field(
+        lower = True, 
+        tokenize = src_tokenizer.tokenize
+    )
+    trg_field = field.Field(
+        lower = True, 
+        tokenize = trg_tokenizer.tokenize, 
+        init_token = '<sos>', 
+        eos_token = '<eos>'
+    )
+    return src_field, trg_field
