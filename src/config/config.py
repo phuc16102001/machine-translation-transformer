265c7fe676e0eea47be8d06611ca920eb14166c7
phuc16102001 <phuc16102001@gmail.com>
2023-02-09 16:44:56 +0700

    Add train code

--- src/config/config.py
+++ src/config/config.py
@@ -2,5 +2,7 @@ config = {
     'max_strlen': 120,
     'batch_size': 1500,
     'd_model': 512,
-    'device': 'cuda'
+    'device': 'cuda',
+    'epoch': 50,
+    'print_every': 500
 }
--- src/train.py
+++ src/train.py
@@ -1,22 +1,108 @@
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
-import torch.nn.functional as F
-import torchtext
+import time
 
-import numpy as np
-import math
-import os
+from config.config import config
 
-import nltk
-import re
-import spacy
-import pandas as pd
+from utils.reader import create_data
+from utils.tokenizer import tokenizer
+from utils.field import create_field
+from utils.dataset import create_dataset
+from utils.scheduler import MyScheduler
+from utils.loss import LabelSmoothingLoss
+from utils.step import step
+from utils.validation import validiate
+
+from modules.transformer import Transformer
+
+def main():
+    device = config['device']
+    d_model = config['d_model']
+    batch_size = config['batch_size']
+    max_strlen = config['max_strlen']
+    n_epoch = config['epoch']
+    print_every = config['print_every']
+
+    print("Loading data")
+    df_train = create_data('data/train.vi', 'data/train.en')
+    df_val = create_data('data/tst2013.vi', 'data/tst2013.en')
+    print(f"Train size: {len(df_train)}")
+    print(f"Validate size: {len(df_val)}")
+
+    print("Creating tokenizer")
+    vi_tokenizer = tokenizer('vi_core_news_lg')
+    en_tokenizer = tokenizer('en_core_web_sm')
+    
+    print("Creating field")
+    src_field, trg_field = create_field(vi_tokenizer, en_tokenizer)
+    src_pad = src_field.vocab.stoi['<pad>']
+    trg_pad = trg_field.vocab.stoi['<pad>']
+
+    print("Construct dataset")
+    train_dataset = create_dataset(df_train, src_field, trg_field)
+    val_dataset = create_dataset(df_val, src_field, trg_field, istrain=False)
+    print(f"Source vocabulary size: {len(src_field.vocab)}")
+    print(f"Target vocabulary size: {len(trg_field.vocab)}")
+
+    print(f"Creating model with d_model = {d_model}")
+    model = Transformer(
+        len(src_field.vocab),
+        len(trg_field.vocab),
+        d_model = d_model,
+        n = 6,
+        heads = 8,
+        dropout = 0.1
+    )
+
+    n_gpu = torch.cuda.device_count()
+    multiple_gpu = n_gpu > 1
+    is_cuda = ('cuda' in device)
+    if (multiple_gpu and is_cuda):
+        print(f"Use {n_gpu} GPUs")
+        model = nn.DataParallel(model)
+    model = model.to(device)
+
+    print("Init parameters")
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+
+    print("Creating optimizer")
+    opt = MyScheduler(
+        torch.optim.Adam(
+            model.parameters(), 
+            betas = (0.9, 0.98), 
+            eps = 1e-9
+        ),
+        0.2, 
+        d_model, 
+        4000
+    )
+
+    print("Creating loss function")
+    criterion = LabelSmoothingLoss(
+        len(trg_field.vocab), 
+        trg_pad_idx = trg_pad, 
+        smoothing = 0.1
+    )
+
+    print("Training begin")
+    for epoch in range(n_epoch):
+        total_loss = 0
+        
+        for i, batch in enumerate(train_dataset): 
+            s = time.time()
+            loss = step(model, opt, batch, criterion)
+            total_loss += loss
+            
+            if (i + 1) % print_every == 0:
+                avg_loss = total_loss / print_every
+                print('epoch: {:03d} - iter: {:05d} - train loss: {:.4f} - time: {:.4f}'.format(epoch, i+1, avg_loss, time.time()- s))
+                total_loss = 0
+                
+        s = time.time()
+        valid_loss = validiate(model, val_dataset, criterion)
+        print('epoch: {:03d} - iter: {:05d} - valid loss: {:.4f} - time: {:.4f}'.format(epoch, i+1, valid_loss, time.time() - s))
 
 if __name__=="__main__":
-    if (torch.cuda.is_available()):
-        device = 'cuda'
-    else:
-        device = 'cpu'
-    print(f"Use {device}")
-    device = torch.device(device)
+    main()
