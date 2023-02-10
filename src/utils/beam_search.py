35f2e747041bcc1224b01d3ab1a3fb28bc4a60e4
phuc16102001 <phuc16102001@gmail.com>
2023-02-11 03:52:25 +0700

    Transfer log score to same device

--- src/utils/beam_search.py
+++ src/utils/beam_search.py
@@ -69,7 +69,7 @@ def beam_search(sentence, model, src_field, trg_field, device, k, max_strlen):
         if (n_finish == k):
             alpha = 0.7
             denom = (sentence_lengths ** alpha).to(device)
-            log_scores = log_scores.view(-1)
+            log_scores = log_scores.view(-1).to(device)
             _, best_idx = torch.max(log_scores / denom)
             break
 
